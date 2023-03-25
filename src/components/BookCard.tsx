10f129679bdf5ba05b78f0758af833d116b690f3
Artemiusz Kuzniecow <arty.kuznetsov@yahoo.com>
2023-03-25 16:09:24 +0300

    finished working with another pages

--- src/components/BookCard.tsx
+++ src/components/BookCard.tsx
@@ -14,7 +14,7 @@ const BookCard = ({ _id }: { _id: string }) => {
   return currentBook ? (
     <div className="shadow-[rgba(0,_0,_0,_0.24)_0px_3px_8px] hover:shadow-[0_8px_30px_rgb(0,0,0,0.12)] gap-2 m-2 w-[300px]  flex flex-col items-center   rounded-xl">
       <div className="bc-light w-full h-[250px] flex justify-center items-center rounded-t-xl">
-        <NavigateWrapper path={`book/${_id}`}>
+        <NavigateWrapper path={`../book/${_id}`}>
           <img src={currentBook.cover} width={150} height="auto" />
         </NavigateWrapper>
       </div>
--- src/components/BookCartCard.tsx
+++ src/components/BookCartCard.tsx
@@ -4,6 +4,7 @@ import FavouriteIcon from "../assets/icons/FavouriteIcon";
 import useCart from "../hooks/useCart";
 import useFavourites from "../hooks/useFavourites";
 import { getBookById } from "../store/books/selectors";
+import NavigateWrapper from "./NavigateWrapper";
 
 const BookCartCard = ({ _id }: { _id: string }) => {
   const currentBook = useSelector(getBookById(_id));
@@ -13,14 +14,16 @@ const BookCartCard = ({ _id }: { _id: string }) => {
   return (
     <>
       <div className="flex justify-between m-3">
-        <div className="flex gap-2">
-          <img src={currentBook?.cover} width={100} />
-          <div className="flex flex-col">
-            <span>{currentBook?.author}</span>
-            <span>{currentBook?.title}</span>
-            <span>{currentBook?.price}</span>
+        <NavigateWrapper path={`../book/${_id}`}>
+          <div className="flex gap-2">
+            <img src={currentBook?.cover} width={100} />
+            <div className="flex flex-col">
+              <span>{currentBook?.author}</span>
+              <span>{currentBook?.title}</span>
+              <span>{currentBook?.price}</span>
+            </div>
           </div>
-        </div>
+        </NavigateWrapper>
         <div className="flex flex-col justify-between">
           <span
             className="text-[25px] text-center hover:bg-gray-100 p-1 rounded-full cursor-pointer"
